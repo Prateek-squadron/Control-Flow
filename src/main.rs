ac0913fe4f3bf9d37213320381497b6bf6157a0d
Prateek-squadron <ps.prateek1709@gmail.com>
2026-02-10 21:08:33 +0530

    practice

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,6 @@
-use std::io;
-
 fn main() {
+    let condition = false;
+    let number = if condition { 5 } else { 6 };
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
-
-    let guess: u64 = guess.trim().parse().expect("Please type a number");
-
-    if guess == 5 {
-        print!("The guess is correct");
-
-    }else if guess == 2{
-            println!("The guess is 2");
-
-    
-    }
-
+    println!("The value of number is: {number}");
 }
